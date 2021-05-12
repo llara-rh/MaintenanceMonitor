661f40a98da4877bbd5b2b6f18bff93b184081c7
LAPTOP-0G3JHJ4A\larar <wi19b083@technikum-wien.at>
2021-05-12 10:56:08 +0200

    Maven set up erstellt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+
+
+public class Main {
+    public static void main(String[] args){
+        System.out.printf(GetMessage());
+    }
+
+    public static String GetMessage(){
+        return "hello world";
+    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.*;
+
+
+public class MainTest {
+    @Test
+    public void test_irgendwas(){
+        //arrange
+
+        //act
+        String actual = Main.GetMessage();
+
+        //assert
+        Assertions.assertEquals("hello world", actual);
+    }
 }
